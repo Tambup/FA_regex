cbcb5c15c2f0f23e9ae68d32510125c0cb402e07
Tambup <alberto.tamburini@hotmail.it>
2020-10-18 22:06:43 +0200

    Commit con versione e configurazioni con maven Sono presenti alcuni test molto superficiali per controllare la struttura del file ispirato a quello presente nelle slide della consegna.

---
+++ src/main/java/com/FA_regex/App.java
@@ -0,0 +1,13 @@
+package com.FA_regex;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class App 
+{
+    public static void main( String[] args ) throws JsonProcessingException, IOException
+    {
+    	//TODO
+    }
+}
